0b9ab2489f7d3a5482cb1b885e5b94473c071b9c
Sharon Kalango <128315178+skalangox@users.noreply.github.com>
2024-05-20 14:32:53 -0400

    Complete mysteriousOrganism.js
    Project Instructions:
    1. Since you need to create multiple objects, create a factory function pAequorFactory() that has two parameters:
    
    The first parameter is a number (no two organisms should have the same number).
    The second parameter is an array of 15 DNA bases.
    pAequorFactory() should return an object that contains the properties specimenNum and dna that correspond to the parameters provided.
    
    
    2. Your team wants you to simulate P. aequor‘s high rate of mutation (change in its DNA).
    
    To simulate a mutation, in pAequorFactory()‘s returned object, add the method .mutate().
    
    .mutate() is responsible for randomly selecting a base in the object’s dna property and changing the current base to a different base. Then .mutate() will return the object’s dna.
    
    For example, if the randomly selected base is the 1st base and it is 'A', the base must be changed to 'T', 'C', or 'G'. But it cannot be 'A' again.
    
    3. Your research team wants to be able to compare the DNA sequences of different P. aequor. You’ll have to add a new method (.compareDNA()) to the returned object of the factory function.
    
    .compareDNA() has one parameter, another pAequor object.
    
    The behavior of .compareDNA() is to compare the current pAequor‘s .dna with the passed in pAequor‘s .dna and compute how many bases are identical and in the same locations. .compareDNA() does not return anything, but prints a message that states the percentage of DNA the two objects have in common — use the .specimenNum to identify which pAequor objects are being compared.
    
    4. P. aequor have a likelier chance of survival if their DNA is made up of at least 60% 'C' or 'G' bases.
    
    In the returned object of pAequorFactory(), add another method .willLikelySurvive().
    
    .willLikelySurvive() returns true if the object’s .dna array contains at least 60% 'C' or 'G' bases. Otherwise, .willLikelySurvive() returns false.
    
    5. With the factory function set up, your team requests that you create 30 instances of pAequor that can survive in their natural environment. Store these instances in an array for your team to study later.
    
    6. Create a .complementStrand() method to the factory function’s object that returns the complementary DNA strand. The rules are that 'A's match with 'T's and vice versa. Also, 'C's match with 'G's and vice versa.

--- mysteriousOrganism.js
+++ mysteriousOrganism.js
@@ -4,7 +4,7 @@ const returnRandBase = () => {
   return dnaBases[Math.floor(Math.random() * 4)] 
 }
 
-// Returns a random single strand of DNA containing 15 bases
+// Returns a random single stand of DNA containing 15 bases
 const mockUpStrand = () => {
   const newStrand = []
   for (let i = 0; i < 15; i++) {
@@ -12,3 +12,122 @@ const mockUpStrand = () => {
   }
   return newStrand
 }
+
+//To ensure no two organisms have the same number
+const loggedSpecimenNumbers = [];
+
+const pAequorFactory = (specimenNum, dna) => {
+  if (loggedSpecimenNumbers.includes(specimenNum)){
+    console.log(`This specimen number is taken!`)
+  } else {
+    loggedSpecimenNumbers.push(specimenNum)
+      return {
+    specimenNum,
+    dna,
+    complementStrand(){
+      let complimentaryDNA = [];
+      for (let i = 0; i < this.dna.length; i++){
+        switch (this.dna[i]){
+          case 'A':
+            complimentaryDNA.push('T');
+            break;
+          case 'T':
+            complimentaryDNA.push('A');
+            break;
+          case 'C':
+            complimentaryDNA.push('G');
+            break;
+          case 'G':
+            complimentaryDNA.push('C');
+            break;
+          default: 
+            complimentaryDNA.push(this.dna[i])                                     
+        }
+      }
+      return `This Specimen's DNA is [${this.dna}] and it's Complimentary DNA is [${complimentaryDNA}]`
+    },
+    willLikelySurvive(){
+      let numOfCandG = 0;
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] !== ('C' || 'G')){
+          continue;
+        }
+        numOfCandG++;
+      }
+      if (((numOfCandG)/15 * 100) >= 60){
+        return true
+      } else { return false }
+    },
+    compareDNA(other){
+      let counter = 0;
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] !== other.dna[i]){
+          continue;
+        }
+        counter++
+      }
+      let percentage = Math.floor((counter/15)*100);
+      return `Specimen #${this.specimenNum} and Specimen #${other.specimenNum} have ${percentage}% DNA in common.`;
+    },
+    mutate(){
+      const randIndex = Math.floor(Math.random() * this.dna.length);
+      let ogBase = this.dna[randIndex];
+      switch (ogBase) {
+        case 'A':
+          this.dna.splice(randIndex, 1, 'T');
+          break;
+        case 'T':
+          this.dna.splice(randIndex, 1, 'C');
+          break;
+        case 'C':
+          this.dna.splice(randIndex, 1, 'G');
+          break;
+        default:
+          this.dna.splice(randIndex, 1, 'A');
+      }
+      return this.dna
+      }   
+    }
+  }}
+
+let organismsThatSurvived = [];
+
+//To create a batch of organisms that will survive in their natural environment
+const pAequorFactoryBatch = number => {
+  let organism = {
+    specimenNum: '',
+    dna: ''
+  };
+  for (let i = 1; organismsThatSurvived.length < number; i++) {
+    organism = pAequorFactory(i,mockUpStrand());
+    let survival = organism.willLikelySurvive();
+    if (survival === true) {
+      organismsThatSurvived.push(organism)
+    }
+    continue;
+  }
+  return organismsThatSurvived
+}
+
+// To create a new dna strand.
+//console.log(mockUpStrand());
+// Then copy results into factory function to keep array constant.
+
+let organism1 = pAequorFactory(1,[ 'A', 'G', 'C', 'C', 'G', 'C', 'C', 'C', 'G', 'A', 'T', 'A', 'A', 'A', 'A' ]);
+
+//let organism2 = pAequorFactory(2,[ 'A', 'A', 'T', 'G', 'A', 'T', 'G', 'T', 'C', 'C', 'T', 'G', 'C', 'G', 'G' ]);
+
+//To check if the mutate method is working.
+//console.log(organism1.mutate());
+
+//To check if the compareDNA method is working.
+//console.log(organism2.compareDNA(organism1));
+
+//To check if the willLikelySurvive method is working.
+//console.log(organism1.willLikelySurvive());
+
+//To check if the pAequorFactoryBatch method is working.
+//console.log(pAequorFactoryBatch(30));
+
+//To check if the complementStrand method is working.
+//console.log(organism1.complementStrand());
